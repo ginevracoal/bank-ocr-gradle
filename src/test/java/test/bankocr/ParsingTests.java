3a35ebf6e6e7606af4372307a0954f7b6b019ede
doma <domagoj.korais@gmail.com>
2019-01-15 10:32:40 +0100

    Refactoring: changed initial class name to capital letter

--- src/test/java/test/bankocr/ParsingTests.java
+++ src/test/java/test/bankocr/ParsingTests.java
@@ -10,7 +10,7 @@ import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.Assert.assertThat;
 
-public class parsingTests {
+public class ParsingTests {
 
 
 

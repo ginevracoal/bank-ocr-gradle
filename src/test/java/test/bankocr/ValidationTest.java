4dee41a5d6cfdab988f59f11afc235c2d94fff25
doma <domagoj.korais@gmail.com>
2019-01-16 11:01:41 +0100

    New tests for Entry::validate

--- src/test/java/test/bankocr/ValidationTest.java
+++ src/test/java/test/bankocr/ValidationTest.java
@@ -3,27 +3,48 @@ package test.bankocr;
 import bankocr.kata.Entry;
 import org.junit.Test;
 
-import java.lang.reflect.Array;
-import java.util.Arrays;
-
-import static com.sun.org.apache.xalan.internal.lib.ExsltStrings.split;
-import static org.hamcrest.core.Is.is;
-import static org.hamcrest.core.IsEqual.equalTo;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class ValidationTest {
 
     @Test
-    public void validationZerosMod11(){
+    public void validationZerosMod11() {
 
-        String [] zeros = {
+        String[] zeros = {
                 " _ ".repeat(9),
                 "| |".repeat(9),
                 "|_|".repeat(9)};
         Entry zerosEntry = new Entry(zeros);
 
         assertTrue(zerosEntry.validate());
+
+    }
+
+    @Test
+    public void validationInvalidMod11() {
+
+        String[] ones = {
+                "   ".repeat(9),
+                "  |".repeat(9),
+                "  |".repeat(9)};
+        Entry onesEntry = new Entry(ones);
+
+        assertFalse(onesEntry.validate());
+
+    }
+
+    @Test
+    public void validationRealisticMod11() {
+
+        String[] realisticString = {
+                "    _  _     _  _  _  _  _ ",
+                "  | _| _||_||_ |_   ||_||_|",
+                "  ||_  _|  | _||_|  ||_| _|"
+        };
+        Entry realisticEntry = new Entry(realisticString);
+
+        assertTrue(realisticEntry.validate());
 
     }
 }
